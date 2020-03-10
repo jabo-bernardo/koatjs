03a5c6ef1796a50df357c72dafe5bf7280c615b5
Code Jabo <joelvincent.work@gmail.com>
2020-03-11 07:56:32 +0800

    💎 Redesigned the system

--- js/koat-v0.1.js
+++ js/koat-v0.1.js
@@ -3,53 +3,69 @@ let Koat = {
 }
 
 elements = document.querySelectorAll(".koat");
-inputTexts = {};
-inputs = [];
-outputs = [];
+data_input_values = {};
+data_input = [];
+data_output = [];
+data_source = [];
+data_foreach = [];
 
 elements.forEach(element => {
-    if(element.getAttribute('data-text:source')) {
-        if(Strings[element.getAttribute("data-text:source")] == undefined)
-            return
-        element.innerHTML = Strings[element.getAttribute('data-text:source')];
+    if(element.getAttribute('data-source')) {
+        data_source.push(element)
     }
     if(element.getAttribute('data-input')) {
-        inputs.push(element);
+        data_input.push(element);
     }
 
     if(element.getAttribute('data-output')) {
-        outputs.push(element);
+        data_output.push(element);
     }
 
     if(element.getAttribute('data-foreach')) {
-        if(!Arrays[element.getAttribute('data-foreach')])
-            return
-        for(let i = 0; i < Arrays[element.getAttribute('data-foreach')].length; i++) {
-            let cln = element.cloneNode(false)
-            let clnd = element.appendChild(cln);
-            clnd.innerHTML = Arrays[element.getAttribute('data-foreach')][i];
-            clnd.removeAttribute('data-foreach');
-        }
+        data_foreach.push(element);
     }
 
 });
 
-let updateOutputs = function() {
-    outputs.forEach(element => {
-        element.innerHTML = inputTexts[element.getAttribute('data-output')];
+let update_data_output = function() {
+    data_output.forEach(element => {
+        element.innerHTML = data_input_values[element.getAttribute('data-output')];
     });
-
-    requestAnimationFrame(updateOutputs)
 }
 
-updateOutputs();
-
-let updateInputs = function() {
-
-    inputs.forEach(element => {
-        inputTexts[element.getAttribute('data-input')] = element.value;
+let update_data_input = function() {
+    data_input.forEach(element => {
+        data_input_values[element.getAttribute('data-input')] = element.value;
     });
-    requestAnimationFrame(updateInputs);
 }
 
-updateInputs();
+let update_data_source = function() {
+    data_source.forEach(element => {
+        if(Strings[element.getAttribute("data-source")] == undefined)
+            return
+        element.innerHTML = Strings[element.getAttribute('data-source')];
+    });
+}
+
+let update_data_foreach = function() {
+    data_foreach.forEach(element => {
+        if(!Arrays[element.getAttribute('data-foreach')])
+            return
+        let content = "";
+        for(let i = 0; i < Arrays[element.getAttribute('data-foreach')].length; i++) {
+            // let cln = element.cloneNode(false)
+            content += "<p>" + Arrays[element.getAttribute('data-foreach')][i] + "</p>"
+            element.innerHTML = content;
+        }
+    })
+}
+
+let updater = function() {
+    update_data_input();
+    update_data_output();
+    update_data_source();
+    update_data_foreach();
+    requestAnimationFrame(updater);
+}
+
+updater();
